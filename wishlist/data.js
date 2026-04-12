4510ccb1aef1b1bcfb60310ba529ce5ac57c095d
Roksana <roksana@growthintelligence.com>
2026-04-12 21:59:40 +0100

    add more plants to wishlist

--- wishlist/data.js
+++ wishlist/data.js
@@ -1,2 +1,69 @@
 // Auto-generated wishlist data.
-window.WISHLIST_DATA = [];
+window.WISHLIST_DATA = [
+  {
+    "id": "alstroemeria-white-pink-blush",
+    "commonName": "Alstroemeria 'White Pink Blush'",
+    "latinName": "Alstroemeria 'White Pink Blush'",
+    "category": "flower",
+    "description": "A beautiful Peruvian Lily with elegant white petals and a soft pink blush. Excellent for cut flowers and borders.",
+    "images": {
+      "plant": "https://www.thompson-morgan.com/product_images/100/ALSTROEMERIA-WHITEPINKBLUSH-KC8081-A.JPG"
+    },
+    "notes": "Requested to be added to wishlist."
+  },
+  {
+    "id": "begonia-apricot-shades",
+    "commonName": "Begonia 'Apricot Shades'",
+    "latinName": "Begonia x tuberhybrida 'Apricot Shades'",
+    "category": "flower",
+    "description": "Large, trailing double blooms in beautiful shades of apricot, orange, and gold. Perfect for hanging baskets and containers.",
+    "images": {
+      "plant": "https://cdn11.bigcommerce.com/s-i7i23daso6/images/stencil/1280w/products/9624/27931/Begonia_Apricot_Shades_Pre-planted_Basket_0012059_3__34696.1639757076.jpg?c=1"
+    },
+    "notes": "Requested to be added to wishlist."
+  },
+  {
+    "id": "gazania-tiger-stripes",
+    "commonName": "Gazania 'Tiger Stripes'",
+    "latinName": "Gazania rigens 'Tiger Stripes'",
+    "category": "flower",
+    "description": "Striking daisy-like flowers with bold contrasting stripes on each petal. Very drought tolerant and loves full sun.",
+    "images": {
+      "plant": "https://apps.rhs.org.uk/plantselectorimages/detail/WSYD0016169.jpg"
+    },
+    "notes": "Requested to be added to wishlist."
+  },
+  {
+    "id": "phlox-pharaoh-blue-eye",
+    "commonName": "Phlox 'Pharaoh Blue Eye'",
+    "latinName": "Phlox subulata 'Pharaoh Blue Eye'",
+    "category": "flower",
+    "description": "A stunning creeping phlox with masses of white flowers, each featuring a striking blue eye. Perfect for rock gardens and ground cover.",
+    "images": {
+      "plant": "https://media.diy.com/is/image/KingfisherDigital/phlox-pharoah-blue-eye-plug-plant-x-50~5057554267453_01c_MP?$MOB_PREV$&$width=1200&$height=1200"
+    },
+    "notes": "Requested to be added to wishlist."
+  },
+  {
+    "id": "saxifraga-peter-pan",
+    "commonName": "Saxifraga 'Peter Pan'",
+    "latinName": "Saxifraga x arendsii 'Peter Pan'",
+    "category": "flower",
+    "description": "A charming, mossy saxifrage forming a low cushion of evergreen foliage, smothered in small, cup-shaped red flowers in spring.",
+    "images": {
+      "plant": "https://apps.rhs.org.uk/plantselectorimages/detail/Web_Use-_KOT4359_9252.jpg"
+    },
+    "notes": "Requested to be added to wishlist."
+  },
+  {
+    "id": "saxifraga-touran-pink",
+    "commonName": "Saxifraga 'Touran Pink'",
+    "latinName": "Saxifraga x arendsii 'Touran Pink'",
+    "category": "flower",
+    "description": "A compact, mound-forming evergreen perennial with vibrant pink flowers that appear in early to late spring.",
+    "images": {
+      "plant": "https://www.gardenia.net/wp-content/uploads/2023/05/Saxifraga-arendsii-Touran-Pink-711x533.webp"
+    },
+    "notes": "Requested to be added to wishlist."
+  }
+];
